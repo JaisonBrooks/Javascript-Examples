4792acabc4265499dd326bb54ea3556377b1aace
Jaison Brooks <4bidd3n403@gmail.com>
2018-08-19 21:27:36 -0600

    updated api sandbox,removed key, oops

--- unit-testing/vat-api-sandbox.js
+++ unit-testing/vat-api-sandbox.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 const result =
   fetch('https://vatapi.com/v1/country-code-check?code=DE', {
     headers: {
-      'apikey': '0e18dc8ee7ff9f678f4681f04543cd90'
+      'apikey': ''
     }
   })
 .then(response => response.json())
